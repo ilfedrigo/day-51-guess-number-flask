cd2c7c9c64a05ce3adf28ed27aad239ddd222c80
Lucas Fedrigo <contato.fedrigo@gmail.com>
2024-02-29 11:52:25 -0300

    Doing some tests with decorator functions

--- main.py
+++ main.py
@@ -2,13 +2,38 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def big_center(function):
+    def wrapper():
+        return f"<h1 style='text-align:center'>{function()}</h1>"
+    return wrapper
+
+def make_bold(function):
+    def wrapper():
+        return f"<b>{function()}<b>"
+    return wrapper
+
+def underline(function):
+    def wrapper():
+        return f"<u>{function()}</u>"
+    return wrapper
+
+def make_italic(function):
+    def wrapper():
+        return f"<em>{function()}</em>"
+    return wrapper
+
 @app.route('/')
 def hello_world():
     return "Hello world!"
 
 @app.route("/bye")
+@big_center
+@make_bold
+@make_italic
+@underline
+
 def bye():
-    return "bye!"
+    return "Bye!"
 
 @app.route("/<name>")
 def greet(name):
